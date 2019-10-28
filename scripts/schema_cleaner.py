a169a02f8bfbbac39c90a6aece87a6f2d02ae70c
Charly Huang <charly.charlongo@gmail.com>
2019-10-28 11:51:54 -0400

    Add Clean_US_flight_delay_dataset class implementation and its calling in main

--- scripts/schema_cleaner.py
+++ scripts/schema_cleaner.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from clean_us_accident_dataset import *
+from clean_us_flight_delay_dataset import *
 
 node_name = 'schema_cleaner'
 
@@ -15,3 +16,18 @@ if __name__=='__main__':
     # The real dataset
     USAccidentDatasetFile = '../us-accidents/US_Accidents_May19.csv'
     Clean_US_Accident_dataset(USAccidentDatasetFile)
+
+
+
+    '''
+    print('Clean US Flight Delay dataset\n')
+
+    # The test dataset
+    # USFlightDelayFile = '/home/charly_huang/Documents/RPI_ITWS_coursework/ITWS 6960 Database Systems/Final Project/DatabaseProject/us-flight-delay/flight_data.csv_trimmed.csv'
+    
+    # The real dataset
+    USFlightDelayFile = '/home/charly_huang/Documents/RPI_ITWS_coursework/ITWS 6960 Database Systems/Final Project/DatabaseProject/us-flight-delay/flight_data.csv'
+    Clean_US_flight_delay_dataset( USFlightDelayFile )
+    '''
+
+    print('{} done !!!!'.format(node_name))
