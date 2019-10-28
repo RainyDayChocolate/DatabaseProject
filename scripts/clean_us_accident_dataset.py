5483b37df57ebf25679d5b645a631e4baf417072
Charly Huang <charly.charlongo@gmail.com>
2019-10-28 11:51:03 -0400

    Complete the implementation on the required features for Clean_US_Accident_dataset

--- scripts/clean_us_accident_dataset.py
+++ scripts/clean_us_accident_dataset.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python
 
 import pandas as pd
+import datetime as dt
 
 class Clean_US_Accident_dataset:
     def __init__(self, inputCSVFile):
-        imported_dataset = pd.read_csv( inputCSVFile )
-        
+        date_attribute = ['Start_Time']
+        imported_dataset = pd.read_csv( inputCSVFile, parse_dates = date_attribute )
+        print('The reeived raw dataset has dimension {}'.format(imported_dataset.size))
+
         cleaned_dataset = imported_dataset[ [ 'Severity'
                                             , 'Start_Time'
                                             , 'City'
@@ -39,5 +42,16 @@ class Clean_US_Accident_dataset:
                                             , 'Astronomical_Twilight'
                                             ]]
 
+        # Keep only tuples with dates falling in 2016
+        print('Keep those tuples which fell into the year 2016...')
+        cleaned_dataset = cleaned_dataset[ cleaned_dataset['Start_Time'].dt.year == int(2016) ]
+        print('Done.')
+
+        # Just for showing
+        print('Showing the first 5 rows of the entire dataset...')
+        print( cleaned_dataset.head() )
+
         outFile = inputCSVFile + '_cleaned_dataset.csv'
+
+        print('The trimmed dataset has dimension {} and saved to \n{}'.format(cleaned_dataset.size, outFile) )
         cleaned_dataset.to_csv(outFile, index = False)
--- scripts/schema_cleaner.py
+++ scripts/schema_cleaner.py
@@ -9,5 +9,9 @@ if __name__=='__main__':
 
     print('Clean the US Accident dataset...')
 
+    # The test dataset
+    # USAccidentDatasetFile = '/home/charly_huang/Documents/RPI_ITWS_coursework/ITWS 6960 Database Systems/Final Project/DatabaseProject/us-accidents/US_Accidents_May19.csv_trimmed.csv'
+
+    # The real dataset
     USAccidentDatasetFile = '../us-accidents/US_Accidents_May19.csv'
     Clean_US_Accident_dataset(USAccidentDatasetFile)
