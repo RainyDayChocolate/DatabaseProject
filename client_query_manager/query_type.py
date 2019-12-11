21680dde68d94bae03a918fe30e4031cc377d737
RainyDayChocolate <likuangzheng@hotmail.com>
2019-12-11 08:30:17 -0500

    Revert "Add package client_query_manager and its unit tests"

--- client_query_manager/query_type.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from enum import Enum, auto
-
-class Query_type(Enum):
-    CAREER_DEP_ARRV_DELAY_IN_AIRPORT = auto()
-    US_ACCIDENT_OCURRENCE = auto()
-    US_GUN_VIOLENCE = auto()
