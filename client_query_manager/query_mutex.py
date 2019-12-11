5067fc8149dcee44dd2b47964d0470879c803034
Charly Huang <charly.charlongo@gmail.com>
2019-12-11 00:26:18 -0500

    Adjust query_mutex to correctly call the methods from both the query_list and specific_queries classes

--- client_query_manager/query_mutex.py
+++ client_query_manager/query_mutex.py
@@ -3,6 +3,7 @@
 from client_query_manager.query_list import Query_list
 from client_query_manager.specific_queries import Specific_queries
 from client_query_manager.query_type import Query_type
+from client_query_manager.helper_class import Helper_class
 
 class Query_Mutex:
     '''
@@ -12,29 +13,33 @@ class Query_Mutex:
     queryType = None
     queryDict = dict()
     queryListInquirer = Query_list()
-    query_inq = Specific_queries()
 
-    def set_query_type(self, queryType):
+    def set_query_type(self, queryType = None):
         '''
-        set the query type for subsequent logic selection
+        set the query type for subsequent logic selection. The queryType is in enum format
+        @ queryType : String. 
         '''
+        if not queryType:
+            raise Exception( 'There is input queryType. queryType must be an enum' )
+
         self.queryType = queryType
+        self.queryListInquirer.create_query_set( self.queryType )
+        self.queryDict = self.queryListInquirer.getQueryHashTable()
     
-    def getQueryList(self, queryType = None):
-        if queryType is None:
-            queryType = self.queryType
-
-        self.queryListInquirer.create_query_set(queryType)
+    def getQueryList(self):
         return self.queryListInquirer.getQueryList()
 
     def get_query_request(self, request):
         '''
         Get the request phrase, so to activate the corresponding query action.
-        @request: The selected phrase by the user. Format: String()
+        @request: The selected phrase by the user. Format: String
         '''
-        self.query_inq.import_query_table( self.queryDict )
-        self.query_inq.setQueryReq(request)
-        return self.query_inq.getQueryResp()
+        queryKey = Helper_class().getKey_from_dict(self.queryDict, request)
+
+        query_inq = Specific_queries()
+        query_inq.import_query_table( self.queryDict )
+
+        query_inq.sendQueryReq(queryKey)
 
     def __str__(self):
         if self.queryType is Query_type.CAREER_DEP_ARRV_DELAY_IN_AIRPORT:
